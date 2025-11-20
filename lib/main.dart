b0014003a52bf71749cbc0af42cd92144246cd18
subinjose-techjays <subinjose.y@techjays.com>
2025-11-20 13:10:46 +0530

    feat: firebase project setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:fin_tracker/core/routes/app_router.dart';
+import 'package:fin_tracker/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ProviderScope(child: FinTrackerApp()));
 }
 
