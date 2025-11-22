f1c0ecfa33ea8d28fcf7aa4c4b903fb39204abee
sharonpjoseph <sharon.joseph@techjays.com>
2025-11-22 16:14:52 +0530

    feat:hardcoded strings removed

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,12 @@
+class AppRoutes {
+  // Route Paths
+  static const String rootPath = '/';
+  static const String loginPath = '/login';
+  static const String dashboardPath = '/dashboard';
+  static const String chatPath = '/chat';
+
+  // Route Names
+  static const String loginName = 'login';
+  static const String dashboardName = 'dashboard';
+  static const String chatName = 'chat';
+}
