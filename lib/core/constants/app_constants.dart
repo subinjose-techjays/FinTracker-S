f7866b2c67ebe2dc9270dbc9d2e548a1238b081c
sharonpjoseph <sharon.joseph@techjays.com>
2025-11-22 16:08:44 +0530

    feat:hardcoded strings removed

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,4 +1,7 @@
 class AppConstants {
   static const int passwordMinLength = 6;
   static const int gemmaMaxTokens = 512;
+  static const String gemmaModelUrl =
+      'https://drive.usercontent.google.com/download?id=1wIbr-IymtH75mt0LK4Z0EHp8ix1OuxSy&export=download&authuser=1&confirm=t&uuid=97ab1524-0786-4e41-b4d7-10facb0ddddb&at=ALWLOp5YTtCYbCb2bk8mgoIaZkAE:1763702562459';
+  static const String gemmaModelFileName = 'gemma-3n-E2B-it-int4.task';
 }
--- lib/features/chat/data/repository/chat_repository_impl.dart
+++ lib/features/chat/data/repository/chat_repository_impl.dart
@@ -16,9 +16,8 @@ class ChatRepositoryImpl implements ChatRepository {
   final Dio _dio;
 
   ChatRepositoryImpl({required Dio dio}) : _dio = dio;
-  final String _modelUrl =
-      'https://drive.usercontent.google.com/download?id=1wIbr-IymtH75mt0LK4Z0EHp8ix1OuxSy&export=download&authuser=1&confirm=t&uuid=97ab1524-0786-4e41-b4d7-10facb0ddddb&at=ALWLOp5YTtCYbCb2bk8mgoIaZkAE:1763702562459';
-  final String _modelFileName = 'gemma-3n-E2B-it-int4.task';
+  final String _modelUrl = AppConstants.gemmaModelUrl;
+  final String _modelFileName = AppConstants.gemmaModelFileName;
 
   @override
   Future<void> initialize() async {
